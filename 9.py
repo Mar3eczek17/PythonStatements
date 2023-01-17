b5bb9caf7348b36137dbf9a50e5354cc8bebea73
Marek Grzesiak <marekgrzesiak.22@gmail.com>
2023-01-17 19:21:47 +0100

    remove comments

--- 9.py
+++ 9.py
@@ -1,8 +1,5 @@
 def animal_crackers(text):
     singiel_word = text.split()
-    # print(a)
-    # print(a[0][0])
-    # print(a[1][0])
     if singiel_word[0][0] == singiel_word[1][0]:
         print(True)
     else:
