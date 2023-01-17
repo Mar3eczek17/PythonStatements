8ce23818e9681a892d2f8c436badf4135cd39972
Marek Grzesiak <marekgrzesiak.22@gmail.com>
2023-01-17 19:33:56 +0100

    file 10

---
+++ 10.py
@@ -0,0 +1,15 @@
+def makes_twenty(n1, n2):
+    if n1 + n2 == 20:
+        return True
+    elif n1 == 20 or n2 == 20:
+        return True
+    else:
+        return False
+
+
+# Check
+print(makes_twenty(20, 10))
+# Check
+print(makes_twenty(12, 8))
+# Check
+print(makes_twenty(2, 3))
